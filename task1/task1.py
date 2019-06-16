62c161992a99fcdd5ae7bb117f2a3801f624d495
Your Name <you@example.com>
2019-06-16 12:47:27 +0300

    Last change

--- task1/task1.py
+++ task1/task1.py
@@ -1,38 +1,35 @@
-list_words, dict_words, new_list = [], {}, []
-for line in open('task1.txt','r'):
-    #new_list.append(line.replace('\n',''))
-    list_words.append(line.replace('\n','').lower().replace(',','').replace('.',' '))
+def main(text_doc='task1.txt'):
+    list_words, dict_words, new_list = [], {}, []
 
-# new_list = ' '.join(new_list).split('.')
-new_list = ' '.join([file.replace('\n','') for file in open('task1.txt', 'r')]).split('.')
+    for line in open(text_doc,'r'):
+        list_words.append(line.replace('\n','').lower().replace(',','').replace('.',' '))
 
-for word in new_list:
-    if word == '':
-        new_list.remove(word)
-print(new_list)
+    new_list = ' '.join([file.replace('\n','') for file in open(text_doc, 'r')]).split('.')
 
+    for word in new_list:
+        if word == '':
+            new_list.remove(word)
 
-list_words = ' '.join(list_words).split(' ')
+    list_words = ' '.join(list_words).split(' ')
 
-for word in list_words:
-    if word == '':
-        list_words.remove(word)
+    for word in list_words:
+        if word == '':
+            list_words.remove(word)
 
-new_spl = ''.join([x[1:] if x.startswith(' ') else x for x in ' '.join(list_words).split('.')]).split(' ')
+    new_spl = ''.join([x[1:] if x.startswith(' ') else x for x in ' '.join(list_words).split('.')]).split(' ')
 
-count_words = len(new_spl) // len(new_list)
+    count_words = len(new_spl) // len(new_list)
 
-for word in list_words:
-    dict_words.update({word: list_words.count(word)})
+    for word in list_words:
+        dict_words.update({word: list_words.count(word)})
 
+    for words,count in dict_words.items():
+        if  str(count) in ['2','3','4']:
+            print(f'Слово - {words} повторяется {count}: разa')
+        else:
+            print(f'Слово - {words} повторяется {count}: раз')
 
-for words,count in dict_words.items():
-    if  str(count) in ['2','3','4']:
-        print(f'Слово - {words} повторяется {count}: разa')
-    else:
-        print(f'Слово - {words} повторяется {count}: раз')
-
-if len(list_words) >= 1:
-    print(f'Среднее количество слов в предложении: {count_words}')
+    if len(list_words) >= 1:
+        print(f'Среднее количество слов в предложении: {count_words}')
 
 
