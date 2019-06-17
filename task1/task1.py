ca908769a1cea2738fac6ec60bb12db0cbab74e7
Your Name <you@example.com>
2019-06-17 23:45:29 +0300

    Last Change

--- task1/task1.py
+++ task1/task1.py
@@ -2,41 +2,46 @@ import re
 from statistics import median
 
 def main():
-    dict_words, text = {}, ''
+    new_spl = []
 
     with open('task1.txt') as file:
-        text += file.read()
+        text = file.read()
 
     # Medians
-    list_with_text = re.split(r'[.?!]',text.replace('\n',''))
+    list_with_text = re.split(r'[.?!]', text.replace('\n', ''))
     for word in list_with_text:
         if word == '':
             list_with_text.remove(word)
 
     median_words = median([len(word.split()) for word in list_with_text])
 
+
     list_words = ' '.join(list_with_text).split(' ')
     for word in list_words:
         if word == '':
             list_words.remove(word)
 
-    new_spl = ''.join([x[1:] if x.startswith(' ') else x for x in ' '.join(list_words).split('.')]).split(' ')
+    for word in ' '.join(list_words).split('.'):
+        if word.startswith(' '):
+            new_spl.append(word[1:])
+        else:
+            new_spl.append(word)
+    new_spl = ''.join(new_spl).split(' ')
 
-    count_words = float("{:0.2f}".format(len(new_spl) / len(list_with_text)))
+    count_words = len(new_spl) / len(list_with_text)
 
-    for word in list_words:
-        dict_words.update({word: list_words.count(word)})
+    dict_words = {word: list_words.count(word) for word in list_words}
 
-    for words,count in dict_words.items():
-        if  str(count)[-1] in ['2','3','4']:
+    for words, count in dict_words.items():
+        if  str(count)[-1] in ['2', '3', '4']:
             print(f'Слово - {words} повторяется: {count} разa')
         else:
             print(f'Слово - {words} повторяется: {count} раз')
 
     if list_words:
-        print(f'Среднее количество слов в предложении: {count_words}')
+        print('Среднее количество слов в предложении: {:0.2f}'.format(float(count_words)))
 
-    print(f"Медианное количество слов в предложение: {median_words}")
+    print('Медианное количество слов в предложение: {}'.format(median_words))
 
 
 if __name__ == '__main__':
