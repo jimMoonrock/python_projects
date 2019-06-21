0671f18cffc2b005ad9ed66f41bb1a61f56a183f
Your Name <you@example.com>
2019-06-21 18:50:07 +0300

    Last Change

--- task2/task2.py
+++ task2/task2.py
@@ -5,16 +5,15 @@ def random_step(n):
     while True:
         increment = yield score
         if increment is not None:
-            score += increment
+            score = increment
         else:
             score += n
 
 r = random_step(5)
-print(next(r))
-# print(r.send(10))
-print(next(r))
-# print(r.send(15))
-print(next(r))
-print(next(r))
-print(next(r))
-
+print(next(r)) # 0
+print(r.send(60)) # 60
+print(next(r)) # None = 60 + 5 (score + n потому что это None)
+print(r.send(123)) # 123
+print(next(r)) # None = 123 + 5 (score + n потому что это None)
+print(next(r)) # None = 128 + 5 (score + n потому что это None)
+print(next(r)) # None = 133 + 5 (score + n потому что это None)
