e6543a98ed8692a0e9514c85afc10b107c2ef819
Your Name <you@example.com>
2019-06-22 16:23:00 +0300

    Last Change

--- task4/task4.py
+++ task4/task4.py
@@ -15,3 +15,15 @@ def palindrome_2(s):
 print(palindrome_2("Madam"))
 
 
+
+def palindrome_3(s):
+    new_str = ''
+    first, last = 0, -1
+    for word in s:
+        if s[first] == s[last]:
+            new_str += word
+            first += 1
+            last -= 1
+    return "Palindrome" if len(new_str) == len(s) else "Not Palindrome"
+
+print(palindrome_3('madam'))
